dbb6408f3706c85383260df2c16655c966f4ca9a
Abhishek Arjun <abhishekarjun2011@gmail.com>
2025-03-06 12:26:42 +0530

    BankingApplication

---
+++ Bankingloginpage/BankApplication2/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace BankApplication2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Username");
+
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter a Password");
+            string password = Console.ReadLine();
+
+            if (username != null && password != null)
+            {
+                if (username == "admin" && password == "admin")
+                {
+                    menu();
+                
+                
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Username or Password");
+                }
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("Username & Password is required");
+            }
+
+            void menu()
+            {
+                Console.WriteLine(":: Bank Menu ::");
+                Console.WriteLine("1. Customer");
+                Console.WriteLine("2. Accounts");
+                Console.WriteLine("3. Transactions");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Enter your choice :");
+                int choice= int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Customer");
+                        break;
+                    case 2:
+                        Console.WriteLine("Accounts");
+                        break;
+                    case 3:
+                        Console.WriteLine("Transactions");
+                        break;
+                    case 0:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+
+
+                }
+
+            }
+
+        }
+        
+    }
+}
---
+++ Bankingloginpage/Bankingloginpage/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel.Design;
+
+namespace Bankingloginpage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("\n:: Login Page ::");
+
+            Console.WriteLine("Enter Usernmae :");
+           string username = Console.ReadLine();
+            Console.WriteLine("Enter Password :");
+           string password = Console.ReadLine();
+
+            if (username != "" && password!="")
+            {
+                
+
+
+                if (username == "system" && password == "manager") 
+                { 
+                 Console.WriteLine("\n Logedin....");
+                    Menu();
+
+
+                }
+                 else
+                 {
+                    Console.WriteLine("Invalid Username or Password");
+
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Username is required");
+
+            }
+
+            void Menu()
+            {
+                Console.WriteLine("1. Create Account");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. Transfer");
+                Console.WriteLine("5. Check Balance");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("Enter your choice :");
+                int switch_on = Convert.ToInt32(Console.ReadLine());
+                switch (switch_on)
+                {
+                   case 1:
+                        Console.WriteLine("Create Account");
+
+                        
+                        break;
+                    case 2:
+                        Console.WriteLine("Deposit");
+                        break;
+                    case 3:
+                        Console.WriteLine("Withdraw");
+                        break;
+                    case 4:
+                        Console.WriteLine("Transfer");
+                        break;
+                    case 5:
+                        Console.WriteLine("Check Balance");
+                        break;
+                    case 6:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
